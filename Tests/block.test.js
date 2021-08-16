de7fb41e2c165a577cceefde8cbb27f1ab15f3dd
Mehul Oswal <mehuloswal21@gmail.com>
2021-08-16 20:07:39 +0530

    ①Genesis Block configured

--- Tests/block.test.js
+++ Tests/block.test.js
@@ -1,4 +1,5 @@
 const Block = require("../BlockchainBackend/Block");
+const { GENESIS_DATA } = require("../config");
 
 describe("Block", () => {
   const timestamp = "a-date";
@@ -17,5 +18,16 @@ describe("Block", () => {
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+  });
+
+  describe("genesis()", () => {
+    const genesisBlock = Block.genesis();
+    console.log("genesis block", genesisBlock);
+    it("returns a block instance", () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+    it("returns the genesis data", () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
   });
 });
