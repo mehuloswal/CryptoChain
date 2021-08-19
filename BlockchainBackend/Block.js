04d3fbce0630a2aa7fd81e0cae59582139ca84d7
Mehul Oswal <mehuloswal21@gmail.com>
2021-08-19 17:50:37 +0530

    Difficulty and the Nonce Value | Code

--- BlockchainBackend/Block.js
+++ BlockchainBackend/Block.js
@@ -2,11 +2,13 @@ const { GENESIS_DATA } = require("../config");
 const cryptoHash = require("./Crypto-hash");
 
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, difficulty, nonce }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
   }
 
   static genesis() {
@@ -14,13 +16,26 @@ class Block {
   }
 
   static mineBlock({ lastBlock, data }) {
-    const timestamp = Date.now();
+    let hash, timestamp;
+    // const timestamp = Date.now();
     const lastHash = lastBlock.hash;
+    const difficulty = lastBlock.difficulty;
+    let nonce = 0;
+
+    do {
+      nonce++;
+      timestamp = Date.now();
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
+
     return new this({
       timestamp: Date.now(),
       lastHash: lastBlock.hash,
       data,
-      hash: cryptoHash(timestamp, lastHash, data),
+      difficulty,
+      nonce,
+      hash,
+      // hash: cryptoHash(timestamp, lastHash, data, nonce, difficulty),
     });
   }
 }
