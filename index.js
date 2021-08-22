28e031feca0659ffe9ca1e31b7076455850385cb
Mehul Oswal <mehuloswal21@gmail.com>
2021-08-22 11:04:59 +0530

    Synchronizing chains at the startup

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const request = require("request");
 const bodyParser = require("body-parser");
 const Blockchain = require("./BlockchainBackend/Blockchain");
 const PubSub = require("./pubsub");
 
 const app = express();
+const DEFAULT_PORT = 3000;
 
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -22,11 +26,28 @@ app.post("/api/mineblock", (req, res) => {
   res.json(blockchain.chain);
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log("replace chain on a sync with", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === "true") {
   PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`listening at localhost:${PORT} !`));
+app.listen(PORT, () => {
+  console.log(`listening at localhost:${PORT} !`);
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
+});
