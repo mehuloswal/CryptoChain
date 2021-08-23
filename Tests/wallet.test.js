a2c6d4a147efa2c7ebbc3ba1494b0d7c56c85ac0
Mehul Oswal <mehuloswal21@gmail.com>
2021-08-23 12:21:11 +0530

    Sign Data and verify Transactions

--- Tests/wallet.test.js
+++ Tests/wallet.test.js
@@ -1,4 +1,5 @@
 const Wallet = require("../Wallet/wallet.js");
+const { verifySignature } = require("../Utils/Elliptic-curve");
 
 describe("Wallet", () => {
   let wallet;
@@ -12,5 +13,27 @@ describe("Wallet", () => {
   });
   it("has a `publicKey`", () => {
     expect(wallet).toHaveProperty("publicKey");
+  });
+
+  describe("signing data", () => {
+    const data = "example";
+    it("verifies a signature", () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data),
+        })
+      ).toBe(true);
+    });
+    it("does not verify an invalid signature", () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data),
+        })
+      ).toBe(false);
+    });
   });
 });
--- Wallet/wallet.js
+++ Wallet/wallet.js
@@ -1,8 +1,15 @@
 const { STARTING_BALANCE } = require("../config");
+const { ec } = require("../Utils/Elliptic-curve");
+const cryptoHash = require("../Utils/Crypto-hash");
 
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE;
+    this.keyPair = ec.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode("hex");
+  }
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data));
   }
 }
 
