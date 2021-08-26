4ec478d75771516a01966af03c1328e3b5dc4b00
Mehul Oswal <mehuloswal21@gmail.com>
2021-08-26 10:17:10 +0530

    Transaction-Miner class

---
+++ App/transaction-miner.js
@@ -0,0 +1,17 @@
+class TransactionMiner {
+  constructor({ blockchain, transactionPool, wallet, pubsub }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.pubsub = pubsub;
+  }
+  mineTransaction() {
+    //get transaction pool's valid transactions
+    //generate the miner's reward
+    //add a block consisting of these transactions to the blockchain
+    //broadcast the update blockchain
+    //clear the pool
+  }
+}
+
+module.exports = TransactionMiner;
