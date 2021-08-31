0313f11d18adb7fc40b0db8419d9255b8fa3fbfe
Mehul Oswal <mehuloswal21@gmail.com>
2021-08-31 16:12:10 +0530

    Wallet Balance calculation done

--- Tests/wallet.test.js
+++ Tests/wallet.test.js
@@ -1,6 +1,8 @@
 const Wallet = require("../Wallet/wallet.js");
 const Transaction = require("../Wallet/transaction");
 const { verifySignature } = require("../Utils/Elliptic-curve");
+const Blockchain = require("../BlockchainBackend/Blockchain");
+const { STARTING_BALANCE } = require("../config.js");
 
 describe("Wallet", () => {
   let wallet;
@@ -64,6 +66,49 @@ describe("Wallet", () => {
       });
       it("outputs the amount to the recipient", () => {
         expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
+    });
+  });
+
+  describe("calculateBalance()", () => {
+    let blockchain;
+    beforeEach(() => {
+      blockchain = new Blockchain();
+    });
+    describe("and there are no outputs for the wallet", () => {
+      it("returns the `STARTING_BALANCE`", () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey,
+          })
+        ).toEqual(STARTING_BALANCE);
+      });
+    });
+    describe("and there are outputs for the wallet", () => {
+      let transactionOne, transactionTwo;
+      beforeEach(() => {
+        transactionOne = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 50,
+        });
+        transactionTwo = new Wallet().createTransaction({
+          recipient: wallet.publicKey,
+          amount: 60,
+        });
+        blockchain.addBlock({ data: [transactionOne, transactionTwo] });
+      });
+      it("adds the sum of all outputs to the wallet balance", () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey,
+          })
+        ).toEqual(
+          STARTING_BALANCE +
+            transactionOne.outputMap[wallet.publicKey] +
+            transactionTwo.outputMap[wallet.publicKey]
+        );
       });
     });
   });
