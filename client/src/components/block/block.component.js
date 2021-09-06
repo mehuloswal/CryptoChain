9ee014247ac8d01b7ec698e511ae58025e1c1a51
Mehul Oswal <mehuloswal21@gmail.com>
2021-09-06 23:19:32 +0530

    Toggling transaction display

--- client/src/components/block/block.component.js
+++ client/src/components/block/block.component.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 export const Block = (props) => {
   const { timestamp, hash, data } = props.block;
-  console.log(props);
-  const hashDisplay = `${hash.substring(0, 15)}...`;
+  const [readMore, setReadMore] = useState(false);
   const stringifiedData = JSON.stringify(data);
-  const dataDisplay =
+  const initialData =
     stringifiedData.length > 35
       ? `${stringifiedData.substring(0, 35)}...`
       : stringifiedData;
+  const [dataToDisplay, setDataToDisplay] = useState(initialData);
+
+  const hashDisplay = `${hash.substring(0, 15)}...`;
+
+  const linkName = readMore ? "Read Less << " : "Read More >> ";
   return (
     <div className="block">
       <div>Hash: {hashDisplay}</div>
       <div>TimeStamp: {new Date(timestamp).toLocaleString()}</div>
-      <div>Data: {dataDisplay}</div>
+      <div>Data: {dataToDisplay}</div>
+      <button
+        onClick={() => {
+          setReadMore(!readMore);
+          if (!readMore) {
+            setDataToDisplay(stringifiedData);
+          } else {
+            setDataToDisplay(initialData);
+          }
+        }}
+        className="button"
+      >
+        {linkName}
+      </button>
     </div>
   );
 };
